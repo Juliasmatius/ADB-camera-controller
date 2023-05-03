9f01be267feb890bb0719cf06c4ab1d5a654e745
Juliasmatius <80146546+Juliasmatius@users.noreply.github.com>
2023-05-03 17:23:16 +0300

    Added files

---
+++ config.py
@@ -0,0 +1,16 @@
+# Camera app package name and main activity name
+# For info with finding it visit : https://android.stackexchange.com/a/248709
+# Note! Preconfigured for OnePlus because I have one of those
+activity_camera_name = "com.oplus.camera/.component.CameraImageActivity"
+
+# Camera app package name and video activity name
+# For info with finding it visit : https://android.stackexchange.com/a/248709
+# Note! Preconfigured for OnePlus because I have one of those
+activity_video_camera_name = "com.oplus.camera/.component.VideoCameraActivity"
+
+# If True then uses wireless adb, if False then uses wired adb
+# NOTE!!! This computer and your phone need to be on the same network.
+is_remote = False
+
+# If wireless adb is enabled put your phone's ip here
+remote_ip = ""
